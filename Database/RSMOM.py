418206353d42491a9fa73da53486c1f3434177be
Manuel Kössler <manuelkossler@Mac.fritz.box>
2026-02-25 15:34:49 +0100

    Force trigger github redeploy for Streamlit Cloud

--- Database/RSMOM.py
+++ Database/RSMOM.py
@@ -12,6 +12,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import io
 
+# Forces a redeploy on github!
 # --- 1. SETUP & THEME ---
 st.set_page_config(page_title="RS Momentum Terminal", layout="wide")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
